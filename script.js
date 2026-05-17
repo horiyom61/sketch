4178f0c64d8b7e6e5715dd870a3372870b91189b
horiyom61 <kocaaydin61@gmail.com>
2026-05-17 22:26:35 +0200

    added the starting dom elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const newGrid = document.getElementById('new-grid');
+const color = document.getElementById('color');
+const random = document.getElementById('random');
+const clear = document.getElementById('clear');
+const grid = document.getElementById('container');
